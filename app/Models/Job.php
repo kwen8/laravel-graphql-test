44ee64499f7723bf77e7bb300b3dc879b49f6d26
chankwen <418415881@qq.com>
2018-03-03 01:05:31 +0800

    修改Job的fillable

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
+    protected $fillable = [
+        'name', 'description',
+    ];
+
     public function user()
     {
         return $this->belongsTo("App\Models\Job");
